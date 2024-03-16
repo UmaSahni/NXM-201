ca591461c9731b0d4cb89c1ec013b10108f79ba9
Uma Sahni <monkeytown64@gmail.com>
2024-03-16 15:13:26 +0530

    Logout from the backend

--- Day-5/Middleware/auth.middleware.js
+++ Day-5/Middleware/auth.middleware.js
@@ -1,11 +1,16 @@
 var jwt = require("jsonwebtoken");
-
+const {blacklist} = require("../blacklist")
 const auth = (req, res, next) => {
     const token = req.headers.authorization
    
   try {
     if(token){
         let extractToken = token.split(" ")[1]
+        // Check if token is present in blacklist array
+        if(blacklist.includes(extractToken)){
+            res.send("Unauthorized. Please login")
+        }
+
         var decoded = jwt.verify(extractToken, 'masai');
         // console.log(decoded)
         next()
